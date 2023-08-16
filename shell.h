00494e9a23d66f31967dcefb405d3a83ee9b5442
acostanahuel <acostanahuel@gmail.com>
2023-08-15 17:54:57 -0700

    update betty things

--- shell.h
+++ shell.h
@@ -14,10 +14,22 @@
 unsigned long int get_num_of_words(const char *string, char delim);
 char **str_to_array(char *buffer, unsigned long int n, char *delim);
 char *check_cmd(char **args, char **patharray, char *argv0);
+char **getpatharray(void);
+char *getpath(void);
+void free_all(char **args, uli nargs, char **path, uli npath, char *input);
+void free_grid(char **grid, int height);
+
+typedef unsigned long int uli;
 
 
+extern char **environ;
 
 
+int fork_and_exec(char **args);
+int _strcmp(const char *str1, const char *str2);
+char *_strcpy(char *dest, const char *src);
+char *_strdup(const char *str);
+char *_strcat(char *dest, char *src);
 
 
 
