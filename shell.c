54508ce913b35c485477cbe7afb4743b7aa559ec
acostanahuel <acostanahuel@gmail.com>
2023-08-15 17:23:50 -0700

    first commit

---
+++ shell.c
@@ -0,0 +1,108 @@
+#include "shell.h"
+
+/**
+ * get_num_of_words - Gets number of words in string
+ * 
+ * @delim: delimiter
+ * @string: string
+ * Return: ul
+ */
+
+unsigned long int get_num_of_words(const char *string, char delim)
+{
+
+
+	int count = 1;
+	char *ptr = NULL;
+	char *ptrcpy = NULL;
+
+	ptr = strdup(string);
+	ptrcpy = ptr;
+
+	while ((ptr = strchr(ptr, delim)) != NULL)
+	{
+		count++;
+		ptr++;
+	}
+
+	free(ptrcpy);
+
+	return (count);
+}
+
+/**
+ * str_to_array - splits a string from imput and 
+ * return an array of each word of the string
+ *
+ *@buffer: buffer 
+ *@n: number of words
+ *@delim: delimiter
+ *
+ * return: pointer to array of strings
+ */
+
+char **str_to_array(char *buffer, unsigned long int n, char *delim)
+{
+	
+	char *currentToken;
+	char **array;
+	int i = 0;
+
+	array = malloc(sizeof(char *) * (n + 1));
+	currentToken = strtok(buffer, delim);
+
+
+	while (currentToken)
+	{
+		array[i] = _strdup(currentToken);
+		currentToken = strtok(NULL, delim);
+		i++;
+	}
+	array[i] = NULL;
+
+	return (array);
+}
+
+
+/**
+ *check_cmd - check existence of cmd command
+ *
+ *@args: arguments
+ *@patharray: pointer to patharray pp to path pointer
+ *return: args or NULL
+ */
+
+
+char *check_cmd(char **args, char **patharray, char *argv0)
+{
+	unsigned long int i;
+	char test[1024];
+	char *path;
+	unsigned long int num_of_words;
+	struct stat st;
+
+	if (stat(args[0], &st) == 0)
+		return (args[0]);
+
+	path = getpath();
+	num_of_words = get_num_of_words(path, ':');
+
+	for (i = 0; i < num_of_words; i++)
+	{
+		strcpy(test, patharray[i]);
+		strcat(test, "/");
+		strcat(test, args[0]);
+
+		if (stat(test, &st) == 0)
+		{
+			free(args[0]);
+			free(path);
+			args[0] = strdup(test);
+			return (args[0]);
+		}
+	}
+	printf(stderr, "%s: 1: %s: not found\n", argv0, args[0]);
+	free(path);
+	free(args[0]);
+	return (NULL);
+}
---
+++ shell.h
@@ -0,0 +1,23 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <string.h>
+#include <limits.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+unsigned long int get_num_of_words(const char *string, char delim);
+
+
+
+
+
+
+
+
+#endif
